e056587d70204184c52a96b736474c3d6504cc38
preranaarchitects <preranaarchitects@gmail.com>
2025-12-07 14:25:02 +0530

    Uploading the changes done on the video view

--- assets/js/dynamicslider.js
+++ assets/js/dynamicslider.js
@@ -1,14 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const sliderData = [
-        { type: 'video', src: './assets/images/video1.mp4' },
-        { type: 'image', src: './assets/images/image1.jpg' },
-        { type: 'image', src: './assets/images/image2.jpg' },
-        { type: 'image', src: './assets/images/image3.jpg' },
-        { type: 'image', src: './assets/images/image4.jpg' },
-        { type: 'image', src: './assets/images/image5.jpg' },
-        { type: 'image', src: './assets/images/image6.jpg' }
+        { type: 'video', src: './assets/images/video-build.mp4' },
+        { type: 'video', src: './assets/images/video-living-area.mp4' },
+        { type: 'video', src: './assets/images/video-staircase.mp4' },
     ];
-
     const sliderContainer = document.querySelector('.hero-slider');
     const heroContent = document.querySelector('.hero-content');
 
